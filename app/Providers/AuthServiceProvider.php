315f1aa1cdc1c5d8d98ec8e6dd12b3f91639b9f3
AmirHossein Darroudi <aho3ein_d79@yahoo.com>
2022-07-19 17:20:41 +0430

    feat: creating a new guard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +30,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Auth::viaRequest('jwt', function (Request $request) {
+            try{
+                $tokenPayload = JWT::decode($request->bearerToken(), new Key(config('jwt.key'), 'HS256'));
+                return User::find($tokenPayload->Sub)->first();
+            } catch(\Exception $th){
+                Log::error($th);
+                return null;
+            }
+        });
     }
 }
